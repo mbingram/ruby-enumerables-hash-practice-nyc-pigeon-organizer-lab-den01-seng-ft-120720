2864909b070186c89ddd3d701a4bb1607072f134
charming-input-2976 <mbingramm@gmail.com>
2020-12-01 18:38:41 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,7 +1,7 @@
 require "pry"
 
 def nyc_pigeon_organizer(data)
-  data.each_with_object({}) do |(key, value), a|
+  data.each_with_object({}) do |(key, value), final_array|
     a[k] = v + 3
   end  
   
